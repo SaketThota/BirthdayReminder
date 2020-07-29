0259bedd93a7fc1c86e72a9b37a87b88b7df12d6
Manjunath0408 <nath.vasam@gmail.com>
2020-07-29 18:50:52 +0530

    Added Remove functionality

--- BR.js
+++ BR.js
@@ -1,19 +1,27 @@
 const Add = document.querySelector(".add"),
+    Remove = document.querySelector(".remove"),
     Name = document.querySelector("#name"),
     Birthdate = document.querySelector("#birthdate"),
     List = document.querySelector(".list");
+ let List_items = document.querySelectorAll(".list-items");
+    // 1 heading
+    // 2 sr name
     
 let Names = [],months = [31,28,31,30,31,30,31,31,30,31,30,31];
+let selected = NaN;
+
+
 
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
     
+    
     let h1=document.createElement('th'),
-        h2=document.createElement('th'),
-        h3 = document.createElement('th'),
-        tr = document.createElement('tr');
+    h2=document.createElement('th'),
+    h3 = document.createElement('th'),
+    tr = document.createElement('tr');
     
     h1.textContent="No.";
     h2.textContent="Name";
@@ -28,15 +36,76 @@ function removeAllChildNodes(parent) {
 
 Add.addEventListener('click',function(){
     let temp = {name:"",date:""};
+    if(Name.value && Birthdate.value){
     temp.name = Name.value;
     temp.date = Birthdate.value;
     Names.push(temp);
+    console.log(List_items)
     showTable();
+    }
 });
 
-function showTable(){        
-    let sz = Names.length;
+function display(){
+    let cnt = 1;
+    Names.forEach(function(ele){
+        let curr = new Date(),
+        currDate = curr.getDate(),
+            currMonth = curr.getMonth();
+            
+            let newbirthday = document.createElement('tr');
+            newbirthday.className="list-items";
+        let newname = document.createElement('td');
+        let newdate = document.createElement('td');
+        let sr = document.createElement('td');
+        newdate.className="list-items-date";
+        sr.className="list-items-date";
+        newname.className="list-items-name";
+        
+        let completeDate = ele.date;
+        // 2020-07-02
+        let birthdayMonth = parseInt(completeDate.substring(5, 7)),
+            birthdayDate = parseInt(completeDate.substring(8, 10));
+            birthdayMonth--;
+        
+            var days = 0;
+            if (currMonth == birthdayMonth) {
+            days += Math.abs(currDate - birthdayDate);
+        } else { 
+            days += Math.abs(months[currMonth] - currDate);
+            currMonth++;
+            let cntMonths = 0;
+            while (cntMonths < Math.abs(currMonth - birthdayMonth)) { 
+                days += months[currMonth];
+                ++cntMonths;
+            }
+            days += birthdayDate;
+        }
+        
+        sr.textContent = cnt++;
+        newname.textContent = ele.name;
+        newdate.textContent = "In " + days + " Days";
+        newbirthday.appendChild(sr);
+        newbirthday.appendChild(newname);
+        newbirthday.appendChild(newdate);
+        if (newname.textContent && newdate.textContent)
+        List.appendChild(newbirthday);
+        List_items = document.querySelectorAll(".list-items");
+        List_items.forEach(function(ele){
+            ele.addEventListener('click',function(){
+                    selected = ele.childNodes[0].textContent;
+            });
+        })
+    })
+}
 
+Remove.addEventListener("click",function(){
+    if(selected!=NaN)List.removeChild(List.childNodes[selected]);
+    if(selected!=NaN)delete Names[selected-1];
+    display();
+    showTable();
+})
+
+function sortTable(){
     Names.sort(function(a,b){
         var mn1 = parseInt(a.date.substring(5,7)); 
         var mn2 = parseInt(b.date.substring(5,7));
@@ -51,54 +120,11 @@ function showTable(){
             else return 1;
         }
     });
+}
 
+function showTable(){        
+    sortTable();
     removeAllChildNodes(List);   
-
-    let cnt = 1;
+    display();
     
-    Names.forEach(function(ele){
-        let curr = new Date(),
-            currDate = curr.getDate(),
-            currMonth = curr.getMonth();
-        
-        let newbirthday = document.createElement('tr');
-        newbirthday.className="list-items";
-        let newname = document.createElement('td');
-        let newdate = document.createElement('td');
-        let sr = document.createElement('td');
-        sr.className = "list-items-name";
-        newname.className = "list-items-name";
-        newdate.className = "list-items-date";
-        
-        let completeDate = ele.date;
-        // 2020-07-02
-        let birthdayMonth = parseInt(completeDate.substring(5, 7)),
-            birthdayDate = parseInt(completeDate.substring(8, 10));
-        birthdayMonth--;
-        
-        var days = 0;
-        if (currMonth == birthdayMonth) {
-            days += Math.abs(currDate - birthdayDate);
-        } else { 
-            days += Math.abs(months[currMonth] - currDate);
-            currMonth++;
-            let cntMonths = 0;
-            console.log(birthdayMonth);
-            console.log(currMonth);
-            while (cntMonths < Math.abs(currMonth - birthdayMonth)) { 
-                days += months[currMonth];
-                ++cntMonths;
-            }
-            days += birthdayDate;
-        }
-
-        sr.textContent = cnt++;
-        newname.textContent = ele.name;
-        newdate.textContent = "In " + days + " Days";
-        newbirthday.appendChild(sr);
-        newbirthday.appendChild(newname);
-        newbirthday.appendChild(newdate);
-        if (newname.textContent && newdate.textContent)
-            List.appendChild(newbirthday);
-    })    
 }
